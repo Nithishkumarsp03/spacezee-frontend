62e3d892c009d2552384edb4df3e2ef37215eee1
mksmmirhs <meraj.exe@gmail.com>
2024-09-21 12:39:00 +0600

    baseurl

--- src/utils/taskJwt.js
+++ src/utils/taskJwt.js
@@ -1,4 +1,4 @@
-import { createSigner } from "fast-jwt";
+import jwt from "jwt-simple";
 
 export const taskJwt = (courseId, email) => {
   const secret = import.meta.env.VITE_JWT_SECRET_KEY;
@@ -8,13 +8,12 @@ export const taskJwt = (courseId, email) => {
     email,
     exp: Math.floor(Date.now() / 1000) + 24 * 60 * 60, // Expires in 1 day
   };
-
+  let token;
   try {
-    const sign = createSigner({ key: secret, algorithm: "HS256" });
-    const token = sign(payload);
+    token = jwt.encode(payload, secret, "HS256");
     return token;
   } catch (error) {
-    console.log("Error generating token:", error);
-    return null;
+    console.log("error token gen", error);
   }
+  return token;
 };
