f1ccc8d3875dae0a9f7b1b0621353a6a0a15eb5b
mksmmirhs <meraj.exe@gmail.com>
2024-09-21 12:01:39 +0600

    token clg

--- src/utils/taskJwt.js
+++ src/utils/taskJwt.js
@@ -4,15 +4,22 @@ export const taskJwt = async (courseId, email) => {
   console.log("token", import.meta.env.VITE_JWT_SECRET_KEY);
 
   const secret = new TextEncoder().encode(import.meta.env.VITE_JWT_SECRET_KEY);
+  console.log("secret", secret);
+
   const payload = {
     courseId,
     email,
   };
-
-  const token = await new SignJWT(payload)
-    .setProtectedHeader({ alg: "HS256" })
-    .setExpirationTime("1d")
-    .sign(secret);
+  console.log("payload", payload);
+  let token;
+  try {
+    token = await new SignJWT(payload)
+      .setProtectedHeader({ alg: "HS256" })
+      .setExpirationTime("1d")
+      .sign(secret);
+  } catch (error) {
+    console.log("error token gen", error);
+  }
 
   return token;
 };
